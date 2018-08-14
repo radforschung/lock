cefd240d5ab83a99b21f238eacc6def01cee2cf6
Maximilian Richt <maxi@richt.name>
2018-08-14 20:17:43 +0200

    try to make it work with the ttgov2 too

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 #include <Bounce2.h>
 
-#define PIN_LOCK_MOTOR 14
-#define PIN_LOCK_ROTATION_SWITCH 12 // (PULLUP!)  LOW: default, HIGH: motor presses against (rotation complete)
-#define PIN_LOCK_LATCH_SWITCH 13  // (PULLUP!) LOW: closed, HIGH: open
+//#define USE_MODEL_NODEMCU32
+#define USE_MODEL_TTGOV2
+
+#ifdef USE_MODEL_NODEMCU32
+  #define PIN_LOCK_MOTOR GPIO_NUM_14
+  #define PIN_LOCK_ROTATION_SWITCH GPIO_NUM_12 // (PULLUP!)  LOW: default, HIGH: motor presses against (rotation complete)
+  #define PIN_LOCK_LATCH_SWITCH GPIO_NUM_13 // (PULLUP!) LOW: closed, HIGH: open
+#endif
+#ifdef USE_MODEL_TTGOV2
+  #define PIN_LOCK_MOTOR GPIO_NUM_15
+  #define PIN_LOCK_ROTATION_SWITCH GPIO_NUM_2 // (PULLUP!)  LOW: default, HIGH: motor presses against (rotation complete)
+  #define PIN_LOCK_LATCH_SWITCH GPIO_NUM_4 // (PULLUP!) LOW: closed, HIGH: open
+#endif
 
 bool firstBoot = true;
 
