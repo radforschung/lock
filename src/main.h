2b64495e9c27d754beb7db5b4341aea23af37f0a
Constantin Müller <ubahnverleih@gmail.com>
2018-09-28 17:20:19 +0200

    include RFIS in main.h

--- src/main.h
+++ src/main.h
@@ -14,11 +14,13 @@
 #include "lock.h"
 #include "lorawan.h"
 #include "serial.h"
+#include "rfid.h"
 
 static const char *TAG = "main";
 const unsigned TX_INTERVAL = 60;
 
 Lock lock = Lock();
 Location location = Location();
+Rfid rfid;// = Rfid();
 
 #endif // _lock_main_h
